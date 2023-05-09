4e44f30c2938a0f08197e90cb59d0d2047153ba2
psiho3 <maxim.vihrev@gmail.com>
2023-05-10 02:36:47 +0300

    /status/?checking_user= added

--- FindFriends/ClientServerCommunication/api/api.py
+++ FindFriends/ClientServerCommunication/api/api.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
+from .serializers import *
 from rest_framework.decorators import action
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from ..models import User, Friendship, Request
-from .serializers import UserSerializer, FriendshipSerializer, RequestSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -21,6 +22,78 @@ class UserViewSet(viewsets.ModelViewSet):
         user_friends_json = UserSerializer(user_friends, many=True)
 
         return Response(user_friends_json .data)
+
+    @action(detail=True, methods=['get'])
+    def incoming(self, request, pk=None):
+        user = self.get_object()
+        incoming_requests_by_user = Request.objects.filter(receiver=user, status='O')
+        incoming_requests_by_user_json = RequestSerializer(incoming_requests_by_user, many=True)
+
+        return Response(incoming_requests_by_user_json.data)
+
+    @action(detail=True, methods=['get'])
+    def outgoing(self, request, pk=None):
+        user = self.get_object()
+        outgoing_requests_by_user = Request.objects.filter(sender=user, status='O')
+        outgoing_requests_by_user_json = RequestSerializer(outgoing_requests_by_user, many=True)
+
+        return Response(outgoing_requests_by_user_json.data)
+
+    @action(detail=True, methods=['get'])
+    def status(self, request, pk=None):
+        user = self.get_object()
+
+        def get_queryset(self, *args, **kwargs):
+            checking_user = self.request.query_params.get('checking_user', None)
+
+            if checking_user:
+                try:
+                    checking_user_object = User.objects.get(username=checking_user)
+                except ObjectDoesNotExist:
+                    checking_user_object = None
+
+                if checking_user_object:
+                    if user.pk < checking_user_object.pk:
+                        try:
+                            checking_friendship = Friendship.objects.get(first_user=user,
+                                                                         second_user=checking_user_object)
+                        except ObjectDoesNotExist:
+                            checking_friendship = None
+                    elif user.pk == checking_user_object.pk:
+                        return Response({'message': 'You are looking at the status of yourself'})
+                    else:
+                        try:
+                            checking_friendship = Friendship.objects.get(first_user=checking_user_object,
+                                                                         second_user=user)
+                        except ObjectDoesNotExist:
+                            checking_friendship = None
+
+                    if checking_friendship:
+                        return Response({'message': 'You are friends'})
+
+                    try:
+                        checking_incoming = Request.objects.get(sender=checking_user_object, receiver=user)
+                    except ObjectDoesNotExist:
+                        checking_incoming = None
+                    if checking_incoming:
+                        return Response({'message': 'This user sent you a friend request but you have'
+                                                    ' not replied to it yet'})
+
+                    try:
+                        checking_outgoing = Request.objects.get(sender=user, receiver=checking_user_object)
+                    except ObjectDoesNotExist:
+                        checking_outgoing = None
+                    if checking_outgoing:
+                        return Response({'message': 'You sent a friend request to this user,'
+                                                    ' but he has not responded to it yet'})
+
+                    return Response({'message': 'You are not affiliated with this user in any way'})
+                else:
+                    return Response({'message': 'This user dose not exists'})
+
+            return Response({'message': 'Specify a user to check by ?checking_user=<user_to_check>'})
+
+        return get_queryset(self)
 
 
 class FriendshipViewSet(viewsets.ModelViewSet):
--- FindFriends/ClientServerCommunication/models.py
+++ FindFriends/ClientServerCommunication/models.py
@@ -1,6 +1,5 @@
-from django.db import models
+from django.db import models, IntegrityError
 from rest_framework import serializers
-from django.core.exceptions import ValidationError
 
 
 class User(models.Model):
@@ -23,6 +22,7 @@ class Friendship(models.Model):
 
     def save(self, *args, **kwargs):
         self.clean()
+
         if self.first_user.pk > self.second_user.pk:
             self.first_user, self.second_user = self.second_user, self.first_user
 
@@ -38,7 +38,28 @@ class Request(models.Model):
     receiver = models.ForeignKey(User, related_name='%(app_label)s_%(class)s_receiver', on_delete=models.CASCADE)
     status = models.CharField(max_length=1, choices=Status.choices, default='O')
 
+    def clean(self):
+        if self.sender == self.receiver:
+            response = serializers.ValidationError({'message': 'You can not send request to yourself'})
+            response.status_code = 200
+
+            raise response
+
     def save(self, *args, **kwargs):
+        self.clean()
+
+        first_user = self.sender
+        second_user = self.receiver
+        if first_user.pk > second_user.pk:
+            first_user, second_user = second_user, first_user
+
+        already_friends = Friendship.objects.filter(first_user=first_user, second_user=second_user)
+        if already_friends.exists():
+            response = serializers.ValidationError({'message': 'You are already friends'})
+            response.status_code = 200
+
+            raise response
+
         reverse = Request.objects.filter(sender=self.receiver, receiver=self.sender)
         if reverse.exists():
             self.status = 'C'
@@ -46,3 +67,4 @@ class Request(models.Model):
             Friendship.objects.create(first_user=self.sender, second_user=self.receiver)
 
         super(Request, self).save(*args, **kwargs)
+
